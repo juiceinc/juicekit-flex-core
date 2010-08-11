d1b216ff7133be3d922498813e7353e67cb60444
CGemignani <cgemignani@chris-gemignanis-macbook-pro-3.local>
2010-08-11 14:03:25 -0400

    Update with hardcoded path to asdoc

--- scripts/docs.bash
+++ scripts/docs.bash
@@ -1,6 +1,7 @@
 # http://gauravj.com/blog/2009/06/setting-asdoc-description-for-packages/
+#alias adoc="/Applications/Adobe Flash Builder 4/sdks/4.1.0/bin//asdoc"
 echo "Run from the main directory of juicekit-flex-core to generate asdocs"
 echo ""
 echo "Output will be generated in target/asdoc-output/"
-asdoc -doc-sources+=src/main/flex -package-description-file scripts/package-description-core.xml -main-title "JuiceKit Core API Documentation" -output target/asdoc-output/
+"/Applications/Adobe Flash Builder 4/sdks/4.1.0/bin//asdoc" -doc-sources+=src/main/flex -package-description-file scripts/package-description-core.xml -main-title "JuiceKit Core API Documentation" -output target/asdoc-output/
 open target/asdoc-output/index.html
