2592d51868698d7ba18989ea059e0b807687c378
CGemignani <cgemignani@chris-gemignanis-macbook-pro-3.local>
2010-08-14 07:34:35 -0400

    Use custom templates in asdoc builds 

--- scripts/docs.bash
+++ scripts/docs.bash
@@ -3,5 +3,5 @@
 echo "Run from the main directory of juicekit-flex-core to generate asdocs"
 echo ""
 echo "Output will be generated in target/asdoc-output/"
-"/Applications/Adobe Flash Builder 4/sdks/4.1.0/bin//asdoc" -doc-sources+=src/main/flex -package-description-file scripts/package-description-core.xml -main-title "JuiceKit Core API Documentation" -output target/asdoc-output/
+"/Applications/Adobe Flash Builder 4/sdks/4.1.0/bin//asdoc" -doc-sources+=src/main/flex -package-description-file scripts/package-description-core.xml -main-title "JuiceKit Core API Documentation" -output target/asdoc-output/ -templates-path scripts/templates
 open target/asdoc-output/index.html
